6e2c73b0871f2ceb0bf997d6eaf432391fafa358
Ioana Matei <matei.ioana1991@gmail.com>
2018-05-18 17:17:39 +0300

    unitTests extra Timeunit

--- testproject/src/main/java/com/proj/Main.java
+++ testproject/src/main/java/com/proj/Main.java
@@ -3,7 +3,16 @@ package com.proj;
 public class Main {
 
     public static void main(String args[]) throws InterruptedException {
+
+        //creare obiect nou - sllep
         Sleep sleepObject = new Sleep();
+
+        //apelare metoda sleep cu parametrii
         sleepObject.sleep("seconds", 10);
+
+        int x= 50;
+        Integer y = new Integer(50);
+
+        System.out.println( y.equals(x));
     }
 }
